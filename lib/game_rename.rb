b5ec0e8ad4f6c1616794e912196882485741398d
Jim Narey <jimnarey@me.com>
2020-11-13 02:16:55 +0000

    Add GameDir class

--- lib/game_rename.rb
+++ lib/game_rename.rb
@@ -3,29 +3,64 @@
 
 require 'optparse'
 
-def parse_command(options)
+# Shorten this and renable linter check
+def parse_command
+  options = {}
   OptionParser.new do |parser|
     parser.banner = 'Usage: hello.rb [options]'
     parser.on('-h', '--help', 'Show this help message') do
       puts parser
     end
-
     parser.on('-i', '--input INPUTDIR',
               "The directory containing a single game's GDI and track files.") do |input_dir_arg|
       options[:input_dir] = input_dir_arg
     end
   end.parse!
+  options
 end
 
-def get_input_files(input_dir)
-  files = Dir.entries(input_dir).select { |f| File.file? File.join(input_dir, f) }
-  # p files
-  files.each do |file|
-    puts File.join(input_dir, file)
+# The directory in which the source files are contained
+class GameDir
+  def initialize(path)
+    @path = path
+    @gdi = get_files_by_type('gdi')
+    puts @gdi
+  end
+
+  def get_files_by_type(extension)
+    Dir.glob("#{@path}/*.#{extension}", File::FNM_CASEFOLD)
   end
 end
 
-options = {}
-parse_command(options)
+def get_input_files(input_dir)
+  Dir.entries(input_dir).select { |f| File.file? File.join(input_dir, f) }
+end
 
-get_input_files(options[:input_dir]) if options[:input_dir]
+def get_file_paths(directory, files)
+  file_paths = []
+  files.each do |file|
+    file_paths.append(File.join(directory, file))
+  end
+  file_paths
+end
+
+# def sort_files(filepaths)
+#   file_collection = {}
+# end
+
+def print_array(input_array)
+  input_array.each do |item|
+    puts item
+  end
+end
+
+options = parse_command
+
+p options[:input_dir]
+
+if options[:input_dir]
+  # files = get_input_files(options[:input_dir])
+  # file_paths = get_file_paths(options[:input_dir], files)
+  # print_array(file_paths)
+  GameDir.new(options[:input_dir])
+end
