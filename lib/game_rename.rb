2a997692f1e81c3932f1d5b183eb0f276631eec9
Jim Narey <jimnarey@me.com>
2020-11-13 00:46:01 +0000

    List files in game_rename

--- lib/game_rename.rb
+++ lib/game_rename.rb
@@ -3,21 +3,30 @@
 
 require 'optparse'
 
-# This will hold the options we parse
-options = {}
+def parse_command(options)
+  OptionParser.new do |parser|
+    parser.banner = 'Usage: hello.rb [options]'
+    parser.on('-h', '--help', 'Show this help message') do
+      puts parser
+    end
 
-OptionParser.new do |parser|
-  parser.banner = 'Usage: hello.rb [options]'
-  parser.on('-h', '--help', 'Show this help message') do
-    puts parser
-  end
-
-  # Whenever we see -n or --name, with an
-  # argument, save the argument.
-  parser.on('-i', '--input INPUTDIR', "The directory containing a single game's GDI and track files.") do |v|
-    options[:name] = v
-  end
-end.parse!
+    parser.on('-i', '--input INPUTDIR',
+              "The directory containing a single game's GDI and track files.") do |input_dir_arg|
+      options[:input_dir] = input_dir_arg
+    end
+  end.parse!
+end
 
 # Now we can use the options hash however we like.
-puts "Hello #{options[:name]}" if options[:name]
+# puts "Input dir is #{options[:input_dir]}" if options[:input_dir]
+
+options = {}
+parse_command(options)
+
+if options[:input_dir]
+  files = Dir.entries(options[:input_dir]).select { |f| File.file? File.join(options[:input_dir], f) }
+  p files
+  files.each do |file|
+    puts File.join(options[:input_dir], file)
+  end
+end
