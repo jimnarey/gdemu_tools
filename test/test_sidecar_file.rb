39bc49a5a10a09b71786d93ef7645f517c1168a6
Jim Narey <jimnarey@me.com>
2020-11-23 16:14:22 +0000

    Add rakefile to run tests

--- test/test_sidecar_file.rb
+++ test/test_sidecar_file.rb
@@ -3,37 +3,35 @@
 require 'minitest/autorun'
 require_relative '../lib/sidecar_file'
 
+# paths = [
+#   '/home/jimnarey/projects/gdemu_tools/test/fixtures/renaming/
+#             gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/',
+#   './fixtures/renaming/gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/', # Not the same as in tryout_paths.rb
+#   './test/fixtures/renaming/gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/',
+#   '/media/sf_G_DRIVE/Dreamcast Games/Sonic Adventure (USA) (En,Ja,Fr,De,Es) (Rev A)/'
+# ]
 
-paths = [
-  '/home/jimnarey/projects/gdemu_tools/test/fixtures/renaming/
-            gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/',
-  './fixtures/renaming/gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/', # Not the same as in tryout_paths.rb 
-  './test/fixtures/renaming/gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/',
-  '/media/sf_G_DRIVE/Dreamcast Games/Sonic Adventure (USA) (En,Ja,Fr,De,Es) (Rev A)/'
-]
+# def test_path(path)
+#   puts Dir.entries(path)
+#   puts format('This path worked: %s', path)
+# rescue StandardError => e
+#   puts e
+#   puts format('This path did not work: %s', path)
+# end
 
-def test_path(path)
-  puts Dir.entries(path)
-  puts format('This path worked: %s', path)
-rescue StandardError => e
-  puts e
-  puts format('This path did not work: %s', path)
-end
-
-paths.each { |path| test_path(path) }
+# paths.each { |path| test_path(path) }
 
 class TestSideCarFile < Minitest::Test
   def test_populates_contents
     expected = [
       '3',
-      '1     0 4 2352 "Sonic Adventure (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 1).bin" 0',
-      '2 14564 0 2352 "Sonic Adventure (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 2).bin" 0',
-      '3 45000 4 2352 "Sonic Adventure (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 3).bin" 0'
+      '1     0 4 2352 "Random Game (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 1).bin" 0',
+      '2 14564 0 2352 "Random Game (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 2).bin" 0',
+      '3 45000 4 2352 "Random Game (USA) (En,Ja,Fr,De,Es) (Rev A) (Track 3).bin" 0'
     ]
 
     # Directory/file not found!
-    sidecar = SideCarFile.new('./fixtures/renaming/
-      gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A).gdi')
+    sidecar = SideCarFile.new('./test/fixtures/renaming/gdi_game/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A)/Random Game (USA) (En,Ja,Fr,De,Es) (Rev A).gdi')
 
     assert_equal expected, sidecar.contents
   end
